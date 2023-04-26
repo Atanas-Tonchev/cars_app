fc85fff4cb6a9cde1c79d5985c52709cda6725b4
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-04-26 15:52:27 +0300

    1.3

--- src/main/java/www/haemimont/cars_app/Main.java
+++ src/main/java/www/haemimont/cars_app/Main.java
@@ -12,20 +12,45 @@ public class Main {
     public static void main(String[] args) throws IOException {
         RandomGenerator generator = new RandomGenerator();
         Calculator calculator = new Calculator();
+        boolean quit = false;
+        String toQuit;
         // get the start time
         long start = System.currentTimeMillis();
 
         // call the method
-        int c = 1000;
+        Scanner scanner = new Scanner(System.in);
+        while (!quit) {
 
-        generator.generateCars(c,2011,2050,"Audi");
-        calculator.Sum(c,2.1);
+            System.out.println("Please input Param:");
+            double param = scanner.nextDouble();
+            System.out.println("Please input Year From:");
+            int yearF = scanner.nextInt();
+            System.out.println("Please input Year To:");
+            int yearT = scanner.nextInt();
+            System.out.println("Please input Model:");
+            String model = scanner.next();
+            System.out.println("Please input Count:");
+            int count = scanner.nextInt();
+            System.out.println("Your results is: ");
+            generator.generateCars(count,yearF,yearT,model);
+            calculator.Sum(count,param);
+            // get the end time
+            long end = System.currentTimeMillis();
 
-        // get the end time
-        long end = System.currentTimeMillis();
+            // execution time
+            long execution = end - start;
+            System.out.println("Execution time: " + execution + " ms");
+            scanner.nextLine();
+            System.out.println("Press 'Q' to quit or any key to continue");
+            toQuit = scanner.next();
+            if (toQuit.equalsIgnoreCase("Q"))
+                quit = true;
 
-        // execution time
-        long execution = end - start;
-        System.out.println("Execution time: " + execution + " ms");
+
+        }
+        scanner.close();
+        System.out.println("Good Bye");
+
+
     }
 }
--- src/main/java/www/haemimont/cars_app/functions/Calculator.java
+++ src/main/java/www/haemimont/cars_app/functions/Calculator.java
@@ -10,9 +10,9 @@ public class Calculator {
     public double Sum(int count,double param){
 
         double price = generator.getRandomPrice();
-        System.out.println("Generated price: " +price);
+        //System.out.println("Generated price: " +price);
         double sum = (price*param)/count;
-        System.out.println("Sum: " +sum);
+        //System.out.println("Sum: " +sum);
         return sum;
 
     }
