4f25a4f3d5ef8b3bffd3e3290e4add7daabf4250
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-05-09 12:00:07 +0300

    2.0

--- src/main/java/www/haemimont/cars_app/myThread/MyCallable.java
+++ src/main/java/www/haemimont/cars_app/myThread/MyCallable.java
@@ -4,10 +4,9 @@ import java.util.concurrent.Callable;
 
 public class MyCallable implements Callable<Double> {
     private final double sum;
-    private final int numTask;
+
     public MyCallable(double sum,int numTask) {
         this.sum = sum;
-        this.numTask = numTask;
     }
 
     @Override
@@ -16,7 +15,4 @@ public class MyCallable implements Callable<Double> {
         return sum;
     }
 
-    public int getNumTask() {
-        return numTask;
-    }
 }
