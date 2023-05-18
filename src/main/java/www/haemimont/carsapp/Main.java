a1ad0743337af0cda5770a044f371313622dc4cc
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-05-18 10:51:55 +0300

    2.2

--- src/main/java/www/haemimont/carsapp/Main.java
+++ src/main/java/www/haemimont/carsapp/Main.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input Param:");
         double param = scanner.nextDouble();
